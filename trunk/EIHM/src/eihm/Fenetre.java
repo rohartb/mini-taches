770ea8893c1577ec6fc1f1d9e558056b40534897
brestaz.florence@gmail.com <brestaz.florence@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-03-15 10:10:04 +0000

    Ajout du MenuBar

--- trunk/EIHM/src/eihm/Fenetre.java
+++ trunk/EIHM/src/eihm/Fenetre.java
@@ -5,13 +5,18 @@ import java.awt.*;
 
 public class Fenetre extends JFrame implements Runnable{
     
+    String name;
     
     public Fenetre(String name){
+          this.name = name;
           SwingUtilities.invokeLater(this);
     }
     
     public void run(){
-                
+              
+        //ajout MenuBar
+        MenuBar menubar = new MenuBar();
+        
         try {
             if (System.getProperty("os.name").equals("Mac OS X")) {
             System.setProperty ("apple.laf.useScreenMenuBar","true");
@@ -23,12 +28,17 @@ public class Fenetre extends JFrame implements Runnable{
             System.out.println("Error setting native LAF: " + e);
         }
         
+        this.setLayout(new BorderLayout());
+        this.add(menubar, BorderLayout.NORTH);
+        this.setTitle(this.name);
         this.pack();
         this.setDefaultLookAndFeelDecorated(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(this.MAXIMIZED_BOTH);
         
-        //frame.setSize(1000,800);
         this.setVisible(true);
+        
+        
     }
     
     
