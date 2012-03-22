d803b71210322215ea8713ac1041e8226eb321be
clercmargo@gmail.com <clercmargo@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-03-22 09:24:26 +0000

    AireDeDessin

---
+++ trunk/EIHM/src/ui/AireDeDessin.java
@@ -0,0 +1,16 @@
+
+package ui;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+public class AireDeDessin extends JComponent {
+
+    public AireDeDessin() {
+    }
+
+    public void PaintComponent(Graphics g){
+        Graphics2D draw = (Graphics2D) g;
+    }
+}
