7e7b7d6dda365aa17e4d10ac32c983a87a1155b6
brestaz.florence@gmail.com <brestaz.florence@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-03-22 09:54:07 +0000

    Ajout des boutons dans BarreOutil

--- EIHM/src/ui/BarreOutil.java
+++ EIHM/src/ui/BarreOutil.java
@@ -5,7 +5,19 @@ import javax.swing.*;
 public class BarreOutil extends JPanel {
     
     public BarreOutil(){
+        JButton nouveau = new JButton("N");
+        JButton sauvegarder = new JButton("S");
+        JButton copier = new JButton("Cop");
+        JButton coller = new JButton("Col");
         
+        this.add(nouveau);
+        this.add(sauvegarder);
+        this.add(copier);
+        this.add(coller);
+        
+        this.setLayout(new GridLayout(0,20));
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
+        this.setVisible(true);
         
     }
     
