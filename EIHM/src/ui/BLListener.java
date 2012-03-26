829239a7465962aa734d33ac78bdbce3d1fbb714
rohartbastien@gmail.com <rohartbastien@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-03-26 17:23:45 +0000

    barrelatListener

---
+++ EIHM/src/ui/BLListener.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author bastien
+ */
+class BLListener implements ActionListener {
+
+    public BLListener() {
+    }
+
+    
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
--- EIHM/src/ui/BarreLaterale.java
+++ EIHM/src/ui/BarreLaterale.java
@@ -5,6 +5,9 @@
  */
 package ui;
 
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -12,10 +15,12 @@ import javax.swing.JPanel;
  * @author bastien
  */
 class BarreLaterale extends JPanel{
-    int largeur;
-    int hauteur;
+    
+    JButton but = new JButton(new ImageIcon("Image/TriangleDroit.png"));
     
     public BarreLaterale(){
-        
+        this.setLayout(new BorderLayout());
+        this.add(but, BorderLayout.EAST);
+        this.setVisible(true);
     }
 }
