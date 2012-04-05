f735ac2479a7dbf9189980b67ac0daf5b1a63976
sophie.penot07@gmail.com <sophie.penot07@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-05 08:31:13 +0000

    barre latérale deplacement

--- trunk/EIHM/src/ui/BarreLaterale.java
+++ trunk/EIHM/src/ui/BarreLaterale.java
@@ -6,10 +6,13 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 /**
  *
@@ -17,13 +20,29 @@ import javax.swing.JPanel;
  */
 class BarreLaterale extends JPanel{
     
-    JButton but = new JButton(new ImageIcon("Image/TriangleDroit.png"));
-    
+    JToggleButton but;
+    JPanel center;
+    JPanel right;
     public BarreLaterale(){
+        
+        but = new JToggleButton("!");
+        
+        center = new JPanel(new BorderLayout());
+        center.setVisible(true);
+        center.add(new JLabel("lalalala"));
+        center.setPreferredSize(new Dimension(115,0));
+        right = new JPanel(new BorderLayout());
+        right.setVisible(true);
+        right.add(but);
+        right.setPreferredSize(new Dimension(15, 0));
+        
         ActionListener ecouteur = new BLListener(this);
+        
         but.addActionListener(ecouteur);
+        
         this.setLayout(new BorderLayout());
-        this.add(but, BorderLayout.EAST);
+        this.add(center, BorderLayout.CENTER);
+        this.add(right,BorderLayout.EAST);
         this.setVisible(true);
     }
 }
