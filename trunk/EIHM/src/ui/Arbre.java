6d37c13c724b79df2f4985ad008d0b76aaad3870
rohartbastien <rohartbastien@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-16 12:31:29 +0000

    noeud

---
+++ trunk/EIHM/src/ui/Arbre.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+/**
+ *
+ * @author bastien
+ */
+public class Arbre { 
+    public final Noeud root;
+    
+    public Arbre(Noeud r){
+        root=r;
+    }
+    
+}
