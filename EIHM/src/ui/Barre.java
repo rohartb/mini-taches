1fee4bc45d0732f8b634d657fa380f682f512520
sophie.penot07 <sophie.penot07@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-19 07:22:18 +0000

    EcouteurOption

--- EIHM/src/ui/Barre.java
+++ EIHM/src/ui/Barre.java
@@ -4,11 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Barre extends JPanel {
-
-    public Barre() {
-        
+    Fenetre f;
+    
+    public Barre(Fenetre f) {
+         this.f = f;
          this.setLayout(new BorderLayout());
-         BarreOutil barreOutil = new BarreOutil();
+         BarreOutil barreOutil = new BarreOutil(f);
          BarrePropriete barrePropriete = new BarrePropriete();
          
          this.add(barreOutil, BorderLayout.NORTH);
---
+++ EIHM/src/ui/EcouteurOption.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author sophie
+ */
+public class EcouteurOption implements ActionListener {
+    Fenetre f;
+
+    public EcouteurOption(Fenetre f) {
+        this.f = f;
+    }
+        
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getActionCommand().equals("nb")){
+            f.changementEtat("nouveau");
+        }
+        else if(ae.getActionCommand().equals("sb")){
+            f.changementEtat("sauvegarder");
+        }
+        else if(ae.getActionCommand().equals("copb")){
+        
+        }
+        else if(ae.getActionCommand().equals("collb")){
+        
+        }
+    }
+    
+}
