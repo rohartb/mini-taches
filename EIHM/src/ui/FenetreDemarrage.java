bb996614d156fe041b88bafcb8547443c8094853
brestaz.florence@gmail.com <brestaz.florence@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-24 07:19:43 +0000

    Suite FenetreDemarrage

--- EIHM/src/ui/FenetreDemarrage.java
+++ EIHM/src/ui/FenetreDemarrage.java
@@ -7,23 +7,36 @@ public class FenetreDemarrage extends JDialog{
 
     Fenetre f;
     
-    public FenetreDemarrage() {
-        this.setTitle("MiniTache");
-        this.setSize(400, 200);
-        this.setLayout(null);
+    public FenetreDemarrage(Fenetre f) {
+        this.f = f;
+        setTitle("MiniTache");
+        setSize(450, 180);
+        setLayout(null);
         
         JLabel label = new JLabel();
-        label.setBorder(BorderFactory.createLineBorder(Color.green));
+        //label.setBorder(BorderFactory.createLineBorder(Color.green));
         label.setText("Bienvenue sur Mini Tache, le logiciel de gestion de tache");
         label.setBounds(10, 10, 350, 20);
         
         JLabel label2 = new JLabel();
-        label2.setBorder(BorderFactory.createLineBorder(Color.black));
+        //label2.setBorder(BorderFactory.createLineBorder(Color.black));
         label2.setText("Vous pouvez :");
         label2.setBounds(10, 30, 350, 20);
         
-        JButton ouvrir = new JButton("Ouvrir");
-        ouvrir.setBounds(10,50, 80, 20);  
+        JButton ouvrir = new JButton("Ouvrir un projet");
+        ouvrir.setBounds(10, 50, 130, 30);
+        ouvrir.addActionListener(new EcouteurFenetreDemarrage(this, this.f));
+        ouvrir.setActionCommand("ouvrir");
+        
+        JButton demo = new JButton("Démonstration");
+        demo.setBounds(150,50, 130, 30);
+        //demo.addActionListener(new EcouteurFenetreDemarrage(this, this.f));
+        //demo.setActionCommand("demo");
+        
+        JButton aide = new JButton("Aide");
+        aide.setBounds(290,50, 130, 30);
+        aide.addActionListener(new EcouteurFenetreDemarrage(this, this.f));
+        aide.setActionCommand("aide");
         
         
         JCheckBox checkBox = new JCheckBox();
@@ -35,12 +48,15 @@ public class FenetreDemarrage extends JDialog{
         checkBox.addActionListener(ecouteurChekBox);
 
         
-        this.add(label);
-        this.add(label2);
-        this.add(checkBox);
-        this.add(ouvrir);
+        add(label);
+        add(label2);
+        add(checkBox);
+        add(ouvrir);
+        add(demo);
+        add(aide);
+        
         //fenetre centré
-        this.setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
     }
     
 }
