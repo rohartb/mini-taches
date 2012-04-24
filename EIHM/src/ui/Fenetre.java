fbb829e2488e1d1813f51bb6a7172952ac7c0a24
clercmargo@gmail.com <clercmargo@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-24 07:58:18 +0000

    plein de truc

--- EIHM/src/ui/Fenetre.java
+++ EIHM/src/ui/Fenetre.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 
-public class Fenetre extends JFrame implements Runnable{
+public class Fenetre extends JFrame implements Runnable {
+
     BarreInfo barreInfo;
     BarreLaterale barreLaterale;
     Barre barre;
@@ -15,41 +16,68 @@ public class Fenetre extends JFrame implements Runnable{
     Boolean menuOn = false;
     Arbre arbre;
     FenetreDemarrage fd;
-    
-    public Fenetre(String name){
-          this.name = name;
+
+    public Fenetre(String name) {
+        this.name = name;
     }
 
-    public void changementEtat(String etat){
-        if(etat.equals("nouveau")){
-            barreInfo.changerStatus("nouveau");
+    public void changementEtat(String etat) {
+        if (etat.equals("nouveau")) {
+            String[] options = {"Sauvegarder", "Continuer sans sauvegarder", "Annuler"};
+            int choix = JOptionPane.showOptionDialog(this, "Nouvel arbre :\n Voulez-vous sauvegarder l'arbre actuel", "Nouveau", 0, JOptionPane.QUESTION_MESSAGE, new ImageIcon("./images/question.png"), options, options[0]);
+
+            if (choix == JOptionPane.YES_OPTION) {
+                changementEtat("sauvegarder");
+                arbre = new Arbre(new Noeud(null, "papa", 0, 0, 0));
+                aire.repaint();
+                barreInfo.info.setText("Nouveau");
+            } else if (choix == JOptionPane.NO_OPTION) {
+                arbre = new Arbre(new Noeud(null, "papa", 0, 0, 0));
+                aire.repaint();
+                barreInfo.info.setText("Nouveau");
+            }
         }
-        if(etat.equals("sauvegarder")){
+        if (etat.equals("sauvegarder")) {
             JFileChooser fc = new JFileChooser();
             fc.showSaveDialog(menubar);
             File selFile = fc.getSelectedFile();
         }
-        if(etat.equals("ouvrir")){
+        if (etat.equals("ouvrir")) {
             JFileChooser fc = new JFileChooser();
             fc.showOpenDialog(menubar);
             File selFile = fc.getSelectedFile();
+            if (selFile != null && arbre.root.getNbFils()!=0) {
+                String[] options = {"Sauvegarder", "Continuer sans sauvegarder", "Annuler"};
+                int choix = JOptionPane.showOptionDialog(this, "Nouvel arbre :\n Voulez-vous sauvegarder l'arbre actuel", "Ouvrir", 0, JOptionPane.QUESTION_MESSAGE, new ImageIcon("./images/question.png"), options, options[0]);
+
+                if (choix == JOptionPane.YES_OPTION) {
+                    changementEtat("sauvegarder");
+                    arbre = new Arbre(new Noeud(null, "papa", 0, 0, 0));
+                    aire.repaint();
+                    barreInfo.info.setText("Nouveau");
+                } else if (choix == JOptionPane.NO_OPTION) {
+                    arbre = new Arbre(new Noeud(null, "papa", 0, 0, 0));
+                    aire.repaint();
+                    barreInfo.info.setText("Nouveau");
+                }
+            }
         }
-        if(etat.equals("aide")){
+        if (etat.equals("aide")) {
             aide.setVisible(true);
         }
         /*if(etat.equals("demo")){
         }*/
     }
 
-    public void run(){        
-        
+    public void run() {
+
         fd = new FenetreDemarrage(this);
-              
+
         //ajout MenuBar
         menubar = new MenuBar(this);
         //ajout BarreOutil
         //BarreOutil barreOutil = new BarreOutil();
-        arbre = new Arbre(new Noeud(null, "Papa", 0,0,0));
+        arbre = new Arbre(new Noeud(null, "Papa", 0, 0, 0));
         aire = new AireDeDessin(this);
         aide = new Aide(this);
         //aire.addMouseListener(new EcouteurNoeud(this));
@@ -59,26 +87,26 @@ public class Fenetre extends JFrame implements Runnable{
         //ajout BarrePropriete
         //BarrePropriete barrePropriete = new BarrePropriete();
         barre = new Barre(this);
-        
-        
-        
+
+
+
         //////////////////////////////////////////////////
         //                                              //
         //          Addaptation de l'apparence          //
         //               par rapport a l'OS             //
         //////////////////////////////////////////////////
         /*try {
-            if (System.getProperty("os.name").equals("Mac OS X")) {
-            System.setProperty ("apple.laf.useScreenMenuBar","true");
-            System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
-            } else {
-                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            }
+        if (System.getProperty("os.name").equals("Mac OS X")) {
+        System.setProperty ("apple.laf.useScreenMenuBar","true");
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Test");
+        } else {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
         } catch(Exception e) {
-            System.out.println("Error setting native LAF: " + e);
+        System.out.println("Error setting native LAF: " + e);
         }*/
-        
-        
+
+
         //////////////////////////////////////////////////
         //                                              //
         //            Création de la fenetre            //
@@ -89,42 +117,42 @@ public class Fenetre extends JFrame implements Runnable{
         setIconImage(icone);
 
         setJMenuBar(menubar);
-        
+
         add(barreLaterale, BorderLayout.WEST);
         //add(barreOutil,BorderLayout.NORTH);
-        add(barre,BorderLayout.NORTH);
-        add(aire,BorderLayout.CENTER);
-        add(barreInfo,BorderLayout.SOUTH);
+        add(barre, BorderLayout.NORTH);
+        add(aire, BorderLayout.CENTER);
+        add(barreInfo, BorderLayout.SOUTH);
         aire.setVisible(true);
-        
+
         setTitle(this.name);
         pack();
         //setDefaultLookAndFeelDecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(this.MAXIMIZED_BOTH);
-        
+
         setVisible(true);
-        
+
         // On vérifie si il faut afficher la fenetre de démarrage
-	String fichier ="config.txt";
-        File config = new File(fichier);                        
-        if(config.exists()){
+        String fichier = "config.txt";
+        File config = new File(fichier);
+        if (config.exists()) {
             //lecture du fichier texte	
-            try{
-                InputStream ips=new FileInputStream(fichier); 
-                InputStreamReader ipsr=new InputStreamReader(ips);
-                BufferedReader br=new BufferedReader(ipsr);
+            try {
+                InputStream ips = new FileInputStream(fichier);
+                InputStreamReader ipsr = new InputStreamReader(ips);
+                BufferedReader br = new BufferedReader(ipsr);
                 String valeurCheckBox;
-                while ((valeurCheckBox=br.readLine())!=null){
-                    if (valeurCheckBox.equals("false")){
+                while ((valeurCheckBox = br.readLine()) != null) {
+                    if (valeurCheckBox.equals("false")) {
                         fd.setVisible(true);
                     }
                 }
-                br.close(); 
-            }catch (Exception e){
+                br.close();
+            } catch (Exception e) {
                 System.out.println(e.toString());
-            }  
-        }else{
+            }
+        } else {
             //Dans le cas du premier lancement de l'application, le fichier config n'existe pas
             //donc on affiche obligatoirement la fenetre
             //Et dans le cas aussi que même si au premier lancement on ne clique pas sur la checkBox
@@ -132,7 +160,4 @@ public class Fenetre extends JFrame implements Runnable{
             fd.setVisible(true);
         }
     }
-    
-    
-    
 }
