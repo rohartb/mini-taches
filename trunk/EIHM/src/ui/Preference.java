dcda445a7730b8399c39a4a8ae1a57a6b1332470
brestaz.florence@gmail.com <brestaz.florence@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-25 09:10:43 +0000

    Ajout bouton "valider" dans FenetreDemarrage + continuations Preference

--- trunk/EIHM/src/ui/Preference.java
+++ trunk/EIHM/src/ui/Preference.java
@@ -1,5 +1,10 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 
 public class Preference extends JDialog {
@@ -9,9 +14,46 @@ public class Preference extends JDialog {
         setSize(500, 410);
         setVisible(true);
         
+        JCheckBox box = new JCheckBox();
+        box.setText("Afficher de nouveau la fenêtre de démarrage");       
+        
+        // On vérifie la valeur dans config.txt pour savoir si le text doit être cliquable ou pas
+        String fichier = "config.txt";
+        File config = new File(fichier);
+        if (config.exists()) {
+            //lecture du fichier texte	
+            try {
+                InputStream ips = new FileInputStream(fichier);
+                InputStreamReader ipsr = new InputStreamReader(ips);
+                BufferedReader br = new BufferedReader(ipsr);
+                String valeurCheckBox;
+                while ((valeurCheckBox = br.readLine()) != null) {
+                    if (valeurCheckBox.equals("true")) {
+                        box.setEnabled(true);
+                    }else{
+                        box.setEnabled(false);
+                    }
+                }
+                br.close();
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }else{
+            box.setEnabled(false);
+        }
+        
+        ActionListener ecouteurPref = new EcouteurPreference();
+        box.addActionListener(ecouteurPref);
+        
+        JButton valider = new JButton("Valider");
+        
+        
+        
+        add(box, BorderLayout.NORTH);
+        add(valider, BorderLayout.SOUTH);
         
         //fenetre centrer
-        this.setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
     }
     
     
