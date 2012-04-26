44a283eb3f18bedb34d3e8efd2f3a650f141559e
clercmargo@gmail.com <clercmargo@gmail.com@e9693951-078c-ee25-2711-e04148bc1845>
2012-04-26 07:01:32 +0000

    Prop

---
+++ trunk/EIHM/src/ui/EcouteurProp.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author bob-eeepc
+ */
+class EcouteurProp implements ActionListener {
+    Propriete p;
+
+    public EcouteurProp(Propriete p) {
+        this.p = p;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("it")){
+            if(p.f.barreLaterale.bl.papa.propriete[0]==0){
+                p.f.barreLaterale.bl.papa.propriete[0]=1;
+            } else {
+                p.f.barreLaterale.bl.papa.propriete[0]=0;
+            }
+        }
+        if(e.getActionCommand().equals("op")){
+            if(p.f.barreLaterale.bl.papa.propriete[1]==0){
+                p.f.barreLaterale.bl.papa.propriete[1]=1;
+            } else {
+                p.f.barreLaterale.bl.papa.propriete[1]=0;
+            }
+        }
+        p.f.barreLaterale.majBouton(p.f.barreLaterale.bl.papa);
+        p.f.aire.repaint();
+    }
+}
